6e536b8b8d6fb6761541f876bcfd13343c8286a1
Nate Tan <nktan93@gmail.com>
2019-05-30 19:37:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   post "/puppy" do
     binding.pry
     @puppy = Puppy.new(params["name"], params["breed"], params["age"])
-    @age = @puppy.age
+    @age = @puppy.months_old
     @breed = @puppy.breed
     @name = @puppy.name
     erb :display_puppy
